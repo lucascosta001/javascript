d9c926663c83a7106199ef94575adb6db12fed90
Lucas Alan Costa Novais <107582861+lucascosta001@users.noreply.github.com>
2023-01-23 23:06:24 -0300

    versão final

--- exercicios/ex001/script.js
+++ exercicios/ex001/script.js
@@ -1,1 +1,22 @@
-alert('Hello!')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = 18//data.getHours()
+    var minuto = data.getMinutes()  
+    msg.innerHTML = `Agora são ${hora}:${minuto} horas.` 
+    if (hora >= 0 && hora < 12) {
+        // Bom Dia
+        img.src = 'imagens/manha.png'
+        document.body.style.background = '#a7f5f5'
+    } else if (hora >= 12 && hora < 18) {
+        // Boa Tarde 
+        img.src = 'imagens/tarde.png'
+        document.body.style.background = '#fed501'
+    } else{
+        // Boa Noite
+        img.src = 'imagens/noite.png'
+        document.body.style.background = '#1475D0' 
+    } 
+}
+
