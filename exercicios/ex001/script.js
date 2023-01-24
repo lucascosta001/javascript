c8aea3126d0577291d22a86876a17c8ee189515f
Lucas Alan Costa Novais <107582861+lucascosta001@users.noreply.github.com>
2023-01-24 19:28:06 -0300

    deixei automático

--- exercicios/ex001/script.js
+++ exercicios/ex001/script.js
@@ -2,7 +2,7 @@ function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
-    var hora = 18//data.getHours()
+    var hora = data.getHours()
     var minuto = data.getMinutes()  
     msg.innerHTML = `Agora são ${hora}:${minuto} horas.` 
     if (hora >= 0 && hora < 12) {
