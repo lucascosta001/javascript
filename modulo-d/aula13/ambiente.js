6ba45230dffa5807c9deb66a49460f8c0c260ef2
Lucas Alan Costa Novais <107582861+lucascosta001@users.noreply.github.com>
2023-01-24 22:46:56 -0300

    Create ambiente.js

---
+++ modulo-d/aula13/ambiente.js
@@ -0,0 +1,22 @@
+var c = 1
+do {
+    console.log(`todo bem? ${c}`)
+    c ++
+} while (c <= 6)
+
+/*
+var c = 1
+while (c <= 6) {
+    console.log(`todo bem? ${c}`)
+    c ++
+}
+*/
+
+/*
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+*/
