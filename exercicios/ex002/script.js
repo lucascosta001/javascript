b2915202cf593c32419b2363ded45e1a4a59c18e
Lucas Alan Costa Novais <107582861+lucascosta001@users.noreply.github.com>
2023-01-31 01:24:52 -0300

    Update script.js

--- exercicios/ex002/script.js
+++ exercicios/ex002/script.js
@@ -3,6 +3,7 @@ function verificar() {
     var ano = data.getFullYear()
     var fano = window.document.getElementById('txtano')
     var res = window.document.querySelector('div#res')
+    
     if (fano.value.length == 0 || Number(fano.value) > ano) {
         window.alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
