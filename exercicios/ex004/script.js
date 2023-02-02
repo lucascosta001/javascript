1a4dc42dc7d831e0982aa4a4c403e9d88ebf57b7
Lucas Alan Costa Novais <107582861+lucascosta001@users.noreply.github.com>
2023-02-02 02:27:44 -0300

    exercício 4

--- exercicios/ex004/script.js
+++ exercicios/ex004/script.js
@@ -1,1 +1,19 @@
-alert('Hello!')
+function gerar() {
+    let n = window.document.getElementById('txtnum')
+    let tab = window.document.getElementById('seltab')
+    
+    if (n.value.length == 0) {
+        alert('Digie um número')
+    } else {
+        let num = Number(n.value)
+        let c = 1
+        tab.innerHTML = ''
+        while (c<=10) {
+            let item = document.createElement('option')
+            item.text = `${num} x ${c} = ${num*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+} 
