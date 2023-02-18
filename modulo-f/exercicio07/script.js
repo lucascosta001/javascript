3e50bcead3e5c11c5b950c16c426ec75ef3e9762
Lucas Alan Costa Novais <107582861+lucascosta001@users.noreply.github.com>
2023-02-18 15:14:20 -0300

    mais uns incrementos

--- modulo-f/exercicio07/script.js
+++ modulo-f/exercicio07/script.js
@@ -1,5 +1,5 @@
 var num = window.document.getElementById('txtnum')
-var res = window.document.getElementById('valor')
+var res = window.document.getElementById('lista')
 var tab = []
 function adicionar(){
      
@@ -8,15 +8,26 @@ function adicionar(){
     } else {
         let n = Number(num.value)
         if(0 < n && n <= 100){
-            tab.push(n)
-            res.innerHTML += ''
+            for (let index = 0;array.length; index++) {
+                const element = array[index];
+                
+            }
+            if (n != tab) {
+                tab.push(n)
             let item = document.createElement('option')
             item.text += `Valor ${n} adicionado`
             item.value += tab.lastIndexOf() 
             res.appendChild(item)
+            } else{
+                alert(`o ${n} já foi adicionado`)
+            }
+            
         } else {
             window.alert('Digite um Número entre 1 e 100')
         }
     }
 }
 
+function finalizar(){
+
+}
