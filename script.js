13d185fe2153d431107a007e960941b7e8563d4c
Lucas Alan Costa Novais <107582861+lucascosta001@users.noreply.github.com>
2023-04-16 01:26:56 -0300

    atualização

--- script.js
+++ script.js
@@ -93,12 +93,12 @@ function gerar() {
     if (n.value.length == 0) {
         alert('Digie um número')
     } else {
-        let num = Number(n.value)
+        let numeros = Number(n.value)
         let c = 1
         tab.innerHTML = ''
         while (c<=10) {
             let item = document.createElement('option')
-            item.text = `${num} x ${c} = ${num*c}`
+            item.text = `${numeros} x ${c} = ${numeros*c}`
             item.value = `tab${c}`
             tab.appendChild(item)
             c++
